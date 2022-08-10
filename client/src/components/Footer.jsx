5e64398355028d1704ec8cb9993a99483f0c36b1
Nicole Favela <nicolefavela@Nicoles-MacBook-Pro-2.local>
2022-08-10 16:49:51 -0600

    added footer and style

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,8 +4,9 @@ const Footer=()=>{
     return(
         <section className="footer">
             <hr/>
-            <section>
-                <a href ="/" target="_blank" >Social</a>
+            <section className="footer-img-credit">
+                <small>image courtesy of</small>
+                <a href ="https://unsplash.com/photos/k4Akpt5-Sfk" target="_blank" > https://unsplash.com/photos/k4Akpt5-Sfk</a>
             </section>
         <section>
 
